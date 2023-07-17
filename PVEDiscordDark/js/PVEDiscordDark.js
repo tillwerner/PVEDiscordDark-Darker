0befb55bb0b922514ef95a0f8c573ef87660c1b1
Till Werner <113259783+tillwerner@users.noreply.github.com>
2023-07-17 20:54:01 +0200

    Update PVEDiscordDark.js

--- PVEDiscordDark/js/PVEDiscordDark.js
+++ PVEDiscordDark/js/PVEDiscordDark.js
@@ -1,5 +1,5 @@
-const COLOR_DARK = '#2C2F33'
-const COLOR_DARKER = '#23272a'
+const COLOR_DARK = '#2C2C2C'
+const COLOR_DARKER = '#ffffff'
 const COLOR_BLURPLE = '#526DD1'
 const COLOR_YELLOW = '#faa61a'
 const COLOR_RED = '#ba2b2d'
